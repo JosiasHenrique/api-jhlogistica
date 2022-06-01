5dfbd807835d14ebc5543cd698c5b521d9d1c445
Josias Henrique <joystickhenrii@gmail.com>
2022-06-01 15:09:36 -0300

    Init project

---
+++ src/main/java/com/api/jhlogistica/JhlogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.api.jhlogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhlogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhlogisticaApplication.class, args);
+	}
+
+}
