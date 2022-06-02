001a752fec029404b5a0878bfe70bf980ab5a0fa
Josias Henrique <joystickhenrii@gmail.com>
2022-06-01 23:45:19 -0300

    releasing cors to the frontend

--- src/main/java/com/api/jhlogistica/config/CorsConfiguration.java
+++ src/main/java/com/api/jhlogistica/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("https://resilient-stroopwafel-6d4058.netlify.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
